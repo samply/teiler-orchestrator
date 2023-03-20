da8d2147f947081e51abbdb028565a3936e20ab9
juarez <d.juarez@dkfz-heidelberg.de>
2023-03-20 21:16:12 +0100

    Only teiler roor config server name configured

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-envsubst  < index.html > temp.html
+envsubst < index.html > temp.html
 mv temp.html index.html
-envsubst  < samply-root-config.js > temp-samply-root-config.js
+envsubst < samply-root-config.js > temp-samply-root-config.js
 mv temp-samply-root-config.js samply-root-config.js
+envsubst '${TEILER_ROOT_CONFIG_SERVER_NAME}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.temp.conf
+mv /etc/nginx/nginx.temp.conf /etc/nginx/nginx.conf
 
 echo 'Start Root Config in NGINX in foreground (non-daemon-mode)'
 exec nginx -g 'daemon off;'
